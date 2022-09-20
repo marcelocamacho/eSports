c0e240a6313a8b663e3f0a5f016bbfd450509e05
MARCELO SANTANA CAMACHO <marcelocamacho.ufpa@gmail.com>
2022-09-20 20:03:26 -0300

    Commit inicial - setup do projeto

---
+++ server/src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/ads', (request, response) => {
+  console.log("Acessou ads")
+  response.json([{id: 1, name:'acessou ads'},{id: 2, name:'acessou ad 2'}])
+})
+
+app.listen(3333)
