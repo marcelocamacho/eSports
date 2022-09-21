047f3719fc32b05bfc95527b5602731434bfd6c5
MARCELO SANTANA CAMACHO <marcelocamacho.ufpa@gmail.com>
2022-09-21 17:08:42 -0300

    Segundo dia: Desenvolvimento da interface mobile com React Native

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,40 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {StatusBar} from 'react-native';
+import {useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black} from '@expo-google-fonts/inter'
+import { Background } from './src/components/Background';
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black
+  })
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello react native!</Text>
-      <Button title='Send 1'></Button>
-      <Button title='Send 2'></Button>
-      <Button title='Send 3'></Button>
-      <Button title='Send 4'></Button>
-      <StatusBar style="auto"/>
-    </View>
-  );
+   <Background>
+    <StatusBar barStyle="light-content"
+      backgroundColor="transparent"
+      translucent></StatusBar>
+      {fontsLoaded ? <Home/> : <Loading/>}
+   </Background>
+  )  
 }
-interface ButtonProps{
-  title:string
-}
-function Button(props: ButtonProps){
-  return (
-    <TouchableOpacity>
-      <Text style={styles.title}>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
-
-const styles = StyleSheet.create({
-  title: {
-    color: '#FFF',
-    fontSize: 21
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Image, FlatList } from 'react-native';
+import logoImg from '../../assets/logo-nlw-esports.png'
+import { GameCard } from '../../components/GameCard';
+import { Heading } from '../../components/Heading';
+import { styles } from './styles';
+import { GAMES } from '../../utils/games';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo}/>
+      <Heading 
+        title="Encontre o seu DUO" 
+        subtitle="Selecione o game que deseja jogar..."/>
+
+      <FlatList
+        data={GAMES}
+        keyExtractor={item => item.id}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({item}) =>(
+          <GameCard data={item}/>
+        )}
+        contentContainerStyle={styles.contentList}
+        />
+
+        
+    </View>
+  );
+}
